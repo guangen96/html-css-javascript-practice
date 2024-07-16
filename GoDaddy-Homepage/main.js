9db5c239d7b4cdd725d12b0dc2273b7049c5e0ea
test <test@example.com>
2024-07-16 18:10:41 +0800

    minor changes to header, add incomplete search bar section

--- GoDaddy-Homepage/main.js
+++ GoDaddy-Homepage/main.js
@@ -1,22 +1,30 @@
 var dropdown = Array.from(document.getElementsByClassName("dropdown"));
 var dropdownOverlay = document.getElementById("dropdown-overlay");
-
-console.log(dropdownOverlay);
+var signin = document.getElementById("signin");
 
 for (i = 0; i < dropdown.length; i++) {
+  // Close dropdown if click outside
+  dropdownOverlay.addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+    if (current.length) {
+      current[0].classList.remove("active");
+      dropdownOverlay.style.display = "none";
+    }
+  });
+
+  // open/close dropdown when button is clicked
   dropdown[i].addEventListener("click", function () {
     // Close dropdown if click button when dropdown is opened
     if (this.classList.contains("active")) {
-      console.log(this.className);
       this.classList.remove("active");
       dropdownOverlay.style.display = "none";
-      console.log(this.className);
       return;
     }
     // Open dropdown if click button
     var current = document.getElementsByClassName("active");
     if (current.length) current[0].classList.remove("active");
     this.classList.add("active");
-    dropdownOverlay.style.display = "block";
+    if (!signin.classList.contains("active"))
+      dropdownOverlay.style.display = "block";
   });
 }
