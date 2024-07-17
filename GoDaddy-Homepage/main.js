9f05e5437c4e327e5a76fb113a75b244a492f93e
test <test@example.com>
2024-07-17 14:41:08 +0800

    add animation for header

--- GoDaddy-Homepage/main.js
+++ GoDaddy-Homepage/main.js
@@ -7,6 +7,8 @@ for (i = 0; i < dropdown.length; i++) {
   dropdownOverlay.addEventListener("click", function () {
     var current = document.getElementsByClassName("active");
     if (current.length) {
+      var target = current[0].querySelectorAll(".bx-chevron-down")[0];
+      target.classList.add("inactive");
       current[0].classList.remove("active");
       dropdownOverlay.style.display = "none";
     }
@@ -14,17 +16,32 @@ for (i = 0; i < dropdown.length; i++) {
 
   // open/close dropdown when button is clicked
   dropdown[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
     // Close dropdown if click button when dropdown is opened
     if (this.classList.contains("active")) {
+      var target = current[0].querySelectorAll(".bx-chevron-down")[0];
+      target.classList.add("inactive");
       this.classList.remove("active");
       dropdownOverlay.style.display = "none";
       return;
     }
     // Open dropdown if click button
+    // Check if dropdown already open
     var current = document.getElementsByClassName("active");
-    if (current.length) current[0].classList.remove("active");
+    // If dropdown already open, remove existing content
+    if (current.length) {
+      var target = current[0].querySelectorAll(".bx-chevron-down")[0];
+      target.classList.add("inactive");
+      current[0].classList.remove("active");
+      this.classList.remove("animation");
+    }
+    // If dropdown not open, add animation
+    else this.classList.add("animation");
     this.classList.add("active");
+    // Add overlay, grey if main nav, white if signin
+    dropdownOverlay.style.display = "block";
+    dropdownOverlay.style.backgroundColor = "rgba(0, 0, 0, 0)";
     if (!signin.classList.contains("active"))
-      dropdownOverlay.style.display = "block";
+      dropdownOverlay.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
   });
 }
