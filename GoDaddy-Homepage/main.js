f49c85b609c9c38a6ace2cc5172c710d0db37a91
test <test@example.com>
2024-07-18 15:34:31 +0800

    added till content 3 section

--- GoDaddy-Homepage/main.js
+++ GoDaddy-Homepage/main.js
@@ -1,13 +1,62 @@
-var dropdown = Array.from(document.getElementsByClassName("dropdown"));
-var dropdownOverlay = document.getElementById("dropdown-overlay");
-var signin = document.getElementById("signin");
+const dropdown = Array.from(document.getElementsByClassName("dropdown"));
+const dropdownOverlay = document.getElementById("dropdown-overlay");
+const signin = document.getElementById("signin");
+const contentSelect = document.getElementsByClassName("content-two-select");
+const contentThreeSelect = document.getElementsByClassName(
+  "content-three-display-card"
+);
 
-for (i = 0; i < dropdown.length; i++) {
+for (let i = 0; i < contentThreeSelect.length; i++) {
+  contentThreeSelect[i].addEventListener("mouseover", function () {
+    const content = document.getElementsByClassName("content-three-display")[0]
+      .children;
+    const current = document
+      .getElementsByClassName("content-three-display")[0]
+      .getElementsByClassName("active");
+    const currentSelect = document.querySelectorAll(
+      ".content-three-display-card.active"
+    );
+
+    // only need execute if hover over not active select
+    if (!content[i].classList.contains("active")) {
+      // remove inactive then add active
+      current[0].classList.remove("active");
+      currentSelect[0].classList.remove("active");
+      contentThreeSelect[0].classList.remove("active");
+
+      contentThreeSelect[i].classList.add("active");
+      content[i].classList.add("active");
+    }
+  });
+}
+
+for (let i = 0; i < contentSelect.length; i++) {
+  contentSelect[i].addEventListener("click", function () {
+    const content = document.getElementsByClassName("content-two-grid");
+    const current = document.querySelectorAll(".content-two-grid.active");
+    const currentSelect = document.querySelectorAll(
+      ".content-two-select.active"
+    );
+
+    // only need execute if click button that's not the active dropdown
+    if (!content[i].classList.contains("active")) {
+      // Remove the inactive one, then add active
+      current[0].classList.remove("active");
+      currentSelect[0].classList.remove("active");
+      contentSelect[0].classList.remove("active");
+
+      contentSelect[i].classList.add("active");
+      content[i].classList.add("active");
+    }
+  });
+}
+
+for (let i = 0; i < dropdown.length; i++) {
   // Close dropdown if click outside
   dropdownOverlay.addEventListener("click", function () {
-    var current = document.getElementsByClassName("active");
+    const current = document.getElementsByClassName("dropdown active");
     if (current.length) {
-      var target = current[0].querySelectorAll(".bx-chevron-down")[0];
+      const target = current[0].querySelectorAll(".bx-chevron-down")[0];
       target.classList.add("inactive");
       current[0].classList.remove("active");
       dropdownOverlay.style.display = "none";
@@ -16,10 +65,10 @@ for (i = 0; i < dropdown.length; i++) {
 
   // open/close dropdown when button is clicked
   dropdown[i].addEventListener("click", function () {
-    var current = document.getElementsByClassName("active");
+    const current = document.getElementsByClassName("dropdown active");
     // Close dropdown if click button when dropdown is opened
     if (this.classList.contains("active")) {
-      var target = current[0].querySelectorAll(".bx-chevron-down")[0];
+      const target = current[0].querySelectorAll(".bx-chevron-down")[0];
       target.classList.add("inactive");
       this.classList.remove("active");
       dropdownOverlay.style.display = "none";
@@ -28,7 +77,7 @@ for (i = 0; i < dropdown.length; i++) {
     // Open dropdown if click button
     // If other dropdown already open, remove existing content
     if (current.length) {
-      var target = current[0].querySelectorAll(".bx-chevron-down")[0];
+      const target = current[0].querySelectorAll(".bx-chevron-down")[0];
       target.classList.add("inactive");
       current[0].classList.remove("active");
       this.classList.remove("animation");
